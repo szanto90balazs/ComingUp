71f6bc57e0b2083491401275ea5ca1bdbbd6489d
Balázs Szántó <balazs.szanto@prezi.com>
2016-05-16 10:53:31 +0200

    don't show the frame counter over my precious hamurger menu

--- ComingUp/App.xaml.cs
+++ ComingUp/App.xaml.cs
@@ -40,7 +40,7 @@ namespace ComingUp
 #if DEBUG
 			if (System.Diagnostics.Debugger.IsAttached)
 			{
-				this.DebugSettings.EnableFrameRateCounter = true;
+				this.DebugSettings.EnableFrameRateCounter = false;
 			}
 #endif
 
